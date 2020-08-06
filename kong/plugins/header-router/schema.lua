cf6ba79a272f815783c232b320da413570754d26
dgorniak <dawidgorniak@gmail.com>
2020-08-07 00:28:10 +0200

    feature(header-router) add support for consumers
    The plugin can be applied to consumers from now on

--- kong/plugins/header-router/schema.lua
+++ kong/plugins/header-router/schema.lua
@@ -6,7 +6,6 @@ local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
 local schema = {
   name = plugin_name,
   fields = {
-    { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer
     { protocols = typedefs.protocols_http },
     { config = {
         type = "record",
